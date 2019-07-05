b3a7cc4a3eb27d2f4b361ccca4e9988f3dec646c
doloresdecabeza <tanialita2308@gmail.com>
2019-07-05 00:02:15 -0400

    Cambio de implementación del método "changeView"

--- src/controllers/HomePetDetailsController.java
+++ src/controllers/HomePetDetailsController.java
@@ -29,7 +29,7 @@ public class HomePetDetailsController implements Initializable {
     @FXML private JFXButton btn5;
     private int currentBtn;
 
-    private Parent[] parent = new Parent[5];
+    private Parent[] parent = new Parent[4];
     private Timeline timeline ;
 
     @Override
@@ -37,11 +37,10 @@ public class HomePetDetailsController implements Initializable {
         currentBtn = 1;
         timeline = new Timeline();
         try{
-            parent[0] = FXMLLoader.load(getClass().getResource("../GUI/Details-Encargado.fxml"));
-            parent[1] = FXMLLoader.load(getClass().getResource("../GUI/Details-Animals.fxml"));
-            parent[2] = FXMLLoader.load(getClass().getResource("../GUI/Details-Animals.fxml"));
-            parent[3] = FXMLLoader.load(getClass().getResource("../GUI/Details-Animals.fxml"));
-            parent[4] = FXMLLoader.load(getClass().getResource("../GUI/Details-Animals.fxml"));
+            parent[0] = FXMLLoader.load(getClass().getResource("../GUI/HomePet/Details/Details-Encargado.fxml"));
+            parent[1] = FXMLLoader.load(getClass().getResource("../GUI/HomePet/Details/Details-Animals.fxml"));
+            parent[2] = FXMLLoader.load(getClass().getResource("../GUI/HomePet/Details/Details-Services.fxml"));
+            parent[3] = FXMLLoader.load(getClass().getResource("../GUI/HomePet/Details/Details-Products.fxml"));
         }catch(IOException e){
             e.printStackTrace();
         }
@@ -49,22 +48,28 @@ public class HomePetDetailsController implements Initializable {
     }
 
      public void changeView(ActionEvent event){
-        int frontOrBack;
         if(currentBtn != Integer.parseInt(String.valueOf(((JFXButton)event.getSource()).getId().charAt(3)))){
-            if(currentBtn < Integer.parseInt(String.valueOf(((JFXButton)event.getSource()).getId().charAt(3)))){
-                parent[Integer.parseInt(String.valueOf(((JFXButton)event.getSource()).getId().charAt(3)))-1].translateYProperty().set(-Main.myStage.getScene().getHeight());
-                frontOrBack = 1;
-            }else{
-                parent[Integer.parseInt(String.valueOf(((JFXButton)event.getSource()).getId().charAt(3)))-1].translateYProperty().set(Main.myStage.getScene().getHeight());
-                frontOrBack = 2;
+            switch(Integer.parseInt(String.valueOf(((JFXButton)event.getSource()).getId().charAt(3)))){
+                case 1:
+                    currentBtn = 0;
+                    break;
+                case 2:
+                    currentBtn = 1;
+                    break;
+                case 3:
+                    currentBtn = 2;
+                    break;
+                case 4:
+                    currentBtn = 3;
+                    break;
             }
-            trasitionPane.getChildren().add(parent[Integer.parseInt(String.valueOf(((JFXButton)event.getSource()).getId().charAt(3)))-frontOrBack]);
-            KeyValue kv = new KeyValue(parent[Integer.parseInt(String.valueOf(((JFXButton)event.getSource()).getId().charAt(3)))-frontOrBack].translateYProperty(),0, Interpolator.EASE_IN);
+            trasitionPane.getChildren().add(parent[currentBtn]);
+            KeyValue kv = new KeyValue(parent[currentBtn].translateYProperty(),0, Interpolator.EASE_IN);
             KeyFrame kf = new KeyFrame(Duration.seconds(1),kv);
             timeline.getKeyFrames().clear();
             timeline.getKeyFrames().add(kf);
-            timeline.setOnFinished(actionEvent -> trasitionPane.getChildren().setAll(parent[currentBtn-frontOrBack]));
-            currentBtn = Integer.parseInt(String.valueOf(((JFXButton)event.getSource()).getId().charAt(3)));
+            timeline.setOnFinished(actionEvent -> trasitionPane.getChildren().setAll(parent[currentBtn]));
+            timeline.setCycleCount(1);
             timeline.play();
         }
 
